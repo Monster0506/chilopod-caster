cad2d87c77c24a73f6a4ca7a0157507764f3b484
Pierre Beyssac <pb@fasterix.frmug.org>
2026-07-02 20:52:17 +0200

    Fix OOM bug. Found by: opencode/glm-5.2

--- caster/packet.c
+++ caster/packet.c
@@ -6,6 +6,8 @@
 
 struct packet *packet_new(size_t len_raw) {
 	struct packet *this = (struct packet *)malloc(sizeof(struct packet) + len_raw);
+	if (this == NULL)
+		return NULL;
 	this->datalen = len_raw;
 	REFCNT_INIT(this);
 	this->rtcm_state = PACKET_RAW;
